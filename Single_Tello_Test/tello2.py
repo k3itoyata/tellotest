ec9f53fc24867c828c76d410036d8f5a46072785
矢田桂都 <yatakeito@makkubukkuea.local>
2022-11-25 10:17:13 +0900

    mama

--- Single_Tello_Test/tello2.py
+++ Single_Tello_Test/tello2.py
@@ -1,15 +1,16 @@
 import socket
 import threading
 import time
-from stats2 import Stats
+from stats import Stats
 
 
 class Tello:
-    def __init__(self):
+    def __init__(self, *ip):
         self.local_ip = ''
         self.local_port = 8889
         self.socket = socket.socket(
             socket.AF_INET, socket.SOCK_DGRAM)  # socket for sending cmd
+        print(self.socket)
         self.socket.bind((self.local_ip, self.local_port))
 
         # thread for receiving cmd ack
@@ -17,12 +18,19 @@ class Tello:
         self.receive_thread.daemon = True
         self.receive_thread.start()
 
-        self.tello_ip = '192.168.10.1'
-        self.tello_port = 8889
-        self.tello_adderss = (self.tello_ip, self.tello_port)
-        self.log = []
+        tello_ips = ip
+        print(tello_ips[0])
+        for i in range(len(tello_ips)):
+            self.tello_ip = tello_ips[i]
+            self.tello_port = 8889
+            name = "self.tello_adderss{} = {}".format(
+                i, (self.tello_ip, self.tello_port))
+            exec(name)
+            print(name)
 
-        self.MAX_TIME_OUT = 15.0
+            self.log = []
+
+            self.MAX_TIME_OUT = 15.0
 
     def send_command(self, command):
         """
@@ -30,25 +38,48 @@ class Tello:
         the last command receives an 'OK'.
         If the command fails (either b/c time out or error),
         will try to resend the command
-        :param command: (str) the command to send
+        :param command: (str) the comman
+        d to send
         :param ip: (str) the ip of Tello
         :return: The latest command response
         """
+        print("--------------------")
+        print(command)
+        print(self.tello_adderss1)
+        print(self.tello_adderss0)
+        print("--------------------")
+
+        def test_a():
+            self.socket.sendto(command.encode('utf-8'), self.tello_adderss0)
+
+        def test_b():
+            self.socket.sendto(command.encode('utf-8'), self.tello_adderss1)
+
+        def test_c():
+            self.socket.sendto(command.encode('utf-8'), self.tello_adderss2)
+
         self.log.append(Stats(command, len(self.log)))
 
-        self.socket.sendto(command.encode('utf-8'), self.tello_adderss)
-        print('sending command: %s to %s' % (command, self.tello_ip))
+        test_a_thread = threading.Thread(target=test_a)
+        test_b_thread = threading.Thread(target=test_b)
+        test_c_thread = threading.Thread(target=test_c)
 
-        start = time.time()
-        while not self.log[-1].got_response():
-            now = time.time()
-            diff = now - start
-            if diff > self.MAX_TIME_OUT:
-                print('Max timeout exceeded... command %s' % command)
-                # TODO: is timeout considered failure or next command still get executed
-                # now, next one got executed
-                return
-        print('Done!!! sent command: %s to %s' % (command, self.tello_ip))
+        test_a_thread.start()
+        test_b_thread.start()
+        test_c_thread.start()
+
+        # print ('sending command: %s to %s' % (command, self.tello_ip))
+
+        # start = time.time()
+        # while not self.log[-1].got_response():
+        #     now = time.time()
+        #     diff = now - start
+        #     if diff > self.MAX_TIME_OUT:
+        #         print ('Max timeout exceeded... command %s' % command)
+        #         # TODO: is timeout considered failure or next command still get executed
+        #         # now, next one got executed
+        #         return
+        # print ('Done!!! sent command: %s to %s' % (command, self.tello_ip))
 
     def _receive_thread(self):
         """Listen to responses from the Tello.
@@ -62,14 +93,14 @@ class Tello:
                 print('from %s: %s' % (ip, self.response))
 
                 self.log[-1].add_response(self.response)
-            except socket.error as exc:  # change
+            except socket.error as exc:
                 print("Caught exception socket.error : %s" % exc)
 
     def on_close(self):
         pass
         # for ip in self.tello_ip_list:
-        self.socket.sendto('land'.encode('utf-8'), (id, 8889))
-        self.socket.close()
+        #     self.socket.sendto('land'.encode('utf-8'), (ip, 8889))
+        # self.socket.close()
 
     def get_log(self):
         return self.log
--- Single_Tello_Test/tello_test2.py
+++ Single_Tello_Test/tello_test2.py
@@ -1,4 +1,4 @@
-from tello2 import Tello
+from tello import Tello
 import sys
 from datetime import datetime
 import time
@@ -6,27 +6,40 @@ import time
 start_time = str(datetime.now())
 
 file_name = sys.argv[1]
+print(file_name)
 
 f = open(file_name, "r")
 commands = f.readlines()
+argv_len = len(sys.argv)  # コマンドラインの数取得
+print(argv_len)
 
-tello = Tello()
-for command in commands:
-    if command != '' and command != '\n':
-        command = command.rstrip()
+try:
+    tello = Tello('10.10.4.175', '10.10.4.176', "10.10.4.186")
+    print(tello)
+    for command in commands:
+        if command != '' and command != '\n':
+            command = command.rstrip()
 
-        if command.find('delay') != -1:
-            sec = float(command.partition('delay')[2])
-            print('delay %s' % sec)
-            time.sleep(sec)
-            pass
-        else:
-            tello.send_command(command)
+            if command.find('delay') != -1:
+                sec = float(command.partition('delay')[2])
+                print
 
-log = tello.get_log()
+                ('delay %s' % sec)
+                time.sleep(sec)
+                pass
+            else:
+                tello.send_command(command)
 
-out = open('log/' + start_time + '.txt', 'w')
-for stat in log:
-    stat.print_stats()
-    str = stat.return_stats()
-    out.write(str)
+        log = tello.get_log()
+
+        out = open('log/' + start_time + '.txt', 'w')
+        for stat in log:
+            stat.print_stats()
+            str = stat.return_stats()
+            out.write(str)
+
+
+except KeyboardInterrupt:
+    tello.send_command("command")
+    # tello.send_command("land")
+    tello.send_command("emergency")
